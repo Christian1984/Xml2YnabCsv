c1dd9ddc3c8b69a420ca5b452c805a5311af0a1f
Christian Hoffmann <chris.hoffmann84@gmail.com>
2014-01-06 13:03:33 +0100

    instances of collection framework removed, code cleanup performed

--- src/implementation/XmlParser.java
+++ src/implementation/XmlParser.java
@@ -1,15 +1,5 @@
 package implementation;
 
-import java.io.StringWriter;
-import java.util.LinkedList;
-import java.util.List;
-
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -32,7 +22,6 @@ public class XmlParser
     
     //private String fileName;
     private Document xmlDoc;
-    //private List<Ntry> ntryList;
     private Ntry[] ntryArray;
     
     private static final String XML_KEY_NTRY = "Ntry";
@@ -70,49 +59,12 @@ public class XmlParser
         return xmlDoc;
     }
     
-    //methods
-    /*private LinkedList<Ntry> buildNtryList()
-    {
-        LinkedList<Ntry> list = new LinkedList<Ntry>();
-        
-        NodeList nodes = xmlDoc.getElementsByTagName(XML_KEY_NTRY);
-        
-        for (int i = 0; i < nodes.getLength(); i++)
-        {
-            Node node = nodes.item(i);
-            Element element = (Element) node;
-           
-            String amount = getValue(new String[] {XML_KEY_AMOUNT}, element);
-            String flow = getValue(new String[] {XML_KEY_FLOW}, element);
-            String date = getValue(new String[] {XML_KEY_BOOKINGDATE, XML_KEY_DATE}, element);
-            String debitor = getValue(new String[] {XML_KEY_NTRYDTLS, XML_KEY_TXDTLS, XML_KEY_RLTDPTIES, XML_KEY_DBTR, XML_KEY_NM}, element);
-            String creditor = getValue(new String[] {XML_KEY_NTRYDTLS, XML_KEY_TXDTLS, XML_KEY_RLTDPTIES, XML_KEY_CDTR, XML_KEY_NM}, element);
-            String memo = getValue(new String[] {XML_KEY_NTRYDTLS, XML_KEY_TXDTLS, XML_KEY_RMTINF, XML_KEY_USTRD}, element);
-            
-            String ntryString = "====================\nNtry:" + "\nAmount: " + amount + "\nFlow: " + flow +
-                "\nDate: " + date + "\nDebitor: " + debitor + "\nCreditor: " + creditor + "\nMemo: " + memo + "\n====================";
-            
-            //System.out.println(ntryString);
-            
-            try
-            {
-                Ntry ntry = new Ntry(date, debitor, creditor, memo, Double.parseDouble(amount), flow.equals(OUTFLOW_INDICATOR)?true:false);
-                list.add(ntry);
-            }
-            catch (Exception ex)
-            {
-                System.out.println("ERROR: The following entry could not be added:\n" + ntryString + "\n" + ex.toString());
-            }
-        }
-        
-        return list;
-    }*/
-    
+    //methods    
     private Ntry[] buildNtryArray()
-    {
-        LinkedList<Ntry> list = new LinkedList<Ntry>();
-        
+    {        
         NodeList nodes = xmlDoc.getElementsByTagName(XML_KEY_NTRY);
+        
+        Ntry[] ntryArray = new Ntry[nodes.getLength()];
         
         for (int i = 0; i < nodes.getLength(); i++)
         {
@@ -134,7 +86,7 @@ public class XmlParser
             try
             {
                 Ntry ntry = new Ntry(date, debitor, creditor, memo, Double.parseDouble(amount), flow.equals(OUTFLOW_INDICATOR)?true:false);
-                list.add(ntry);
+                ntryArray[i] = ntry;
             }
             catch (Exception ex)
             {
@@ -142,17 +94,8 @@ public class XmlParser
             }
         }
         
-        //return list.toArray(new Ntry[0]);
-        return list.toArray(new Ntry[list.size()]);
+        return ntryArray;
     }
-    
-    /*private String getValue(String key, Element e)
-    {
-        Node node = e.getElementsByTagName(key).item(0).getChildNodes().item(0);
-        //System.out.println(node.getNodeName());
-        //System.out.println(node.getChildNodes().item(0));
-        return node.getNodeValue();
-    }*/
     
     private String getValue(String[] keyList, Element e)
     {
@@ -177,32 +120,10 @@ public class XmlParser
     }
     
     //output
-    public String xmlDocToString()
-    {
-        TransformerFactory tf = TransformerFactory.newInstance();
-        Transformer transformer;
-        
-        try {
-            transformer = tf.newTransformer();
-            
-            // below code to remove XML declaration
-            // transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
-            StringWriter writer = new StringWriter();
-            transformer.transform(new DOMSource(xmlDoc), new StreamResult(writer));
-            String output = writer.getBuffer().toString();
-            return output;
-        } catch (TransformerException e) {
-            e.printStackTrace();
-        }
-         
-        return null;
-    }
-    
     public String toString()
     {
         String s = "Date,Payee,Category,Memo,Outflow,Inflow\n";
         
-        //for (Ntry ntry : ntryList)
         for (Ntry ntry : ntryArray)
         {
             s += ntry.toString() + "\n";
